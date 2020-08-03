32ade9ab1da41d5bd32479946c8d395af80de4ee
LorD_222 <ilya.r.200594@gmail.com>
2020-08-03 14:57:04 +0300

    update

--- ADV-TV/lesson-13-input.py
+++ ADV-TV/lesson-13-input.py
@@ -6,7 +6,7 @@ num1= input("Enter X: ")
 num2= input("Enter Y: ")
 summa = int(num1)+int(num2) # дествия с введеными цифрами
 print(summa)
-message =""
+message =" "
 while True: # бесконечный цикл с выходом из него при правильном вводе пароля
   message = input("enter Password ")
     if message =='sekret':
